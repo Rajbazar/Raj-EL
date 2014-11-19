30fe02efa854ee29c2b34fb2bc27d752ede87ec3
RajBazar <rajbazar@gmail.com>
2014-11-19 15:56:01 +0000

    HUB
    Initial Setup commit.

---
+++ HUB/features/demo.feature
@@ -0,0 +1,25 @@
+Feature: Demo Feature
+   As a HUB app user
+   I want to navigate the app from home screen
+   So that I can  see all the functions working as per expected 
+
+@Android @HUB-00
+ Scenario: As a valid app user for HUB
+ When I press "Login"
+ Then I see "Welcome to coolest app ever"
+  
+
+@Android @HUB-01
+Scenario: Tapping elements throught the App
+ Given Session S1. I tap on LeftMenu
+  Then Session S1. I tap on myStay
+  When Session S1. I tap on LeftMenu
+  Then Session S1. I tap on myBookings
+  When Session S1. I tap on LeftMenu
+  Then Session S1. I tap on myAccount
+  When Session S1. I tap on leftMenu
+  Then Session S1. I tap on info
+  When Session S1. I tap on LeftMenu
+  Then Session S1. I tap on debug
+  When Session S1. I tap on LeftMenu
+  Then Session S1. Display AppVersion
---
+++ HUB/features/step_definitions/calabash_steps.rb
@@ -0,0 +1,33 @@
+require 'calabash-android/calabash_steps'
+
+##Given Session S1. I tap on LeftMenu
+Then(/^Session ([\w\d]+). I tap on ([\w ]+)$/) do |session,ops|
+  set_default_device($session[session])
+  sleep 5
+  if ops == "LeftMenu"
+  	tap_mark $id_config["left_menu"]
+  elsif ops == "myStay"
+  	tap_mark $id_config["left_menu_mystay"]	
+  elsif ops == "myBookings"
+  	tap_mark $id_config["left_menu_mybookings"]	
+  elsif ops == "myAccount"
+  	tap_mark $id_config["left_menu_myaccount"]
+  elsif ops == "info"
+    tap_mark $id_config["left_menu_info"] 
+  elsif ops == "debug"
+    tap_mark $id_config["left_menu_debug"] 							
+  else
+  	puts "ID not Found"
+  end
+ end 	
+
+ ##Then Session S1. Display AppVersion
+Then(/^Session ([\w\d]+). Display ([\w ]+)$/) do |session,ops|
+ 	set_default_device($session[session])
+  	sleep 3
+  	if ops == "AppVersion"
+  		puts query("* id:'"+$id_config["left_menu_version"]+"'", :text)[0]
+  	else
+  		puts "ID not Found"
+	end
+end
