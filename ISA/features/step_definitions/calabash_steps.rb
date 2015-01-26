0150c367b18c03bf4839c9c64cc69894344c6766
prateekjagota <Prateek@Prateeks-MacBook-Pro.local>
2015-01-26 21:12:15 +0530

    26 jan part 2
    Added code for android with app issues (Not Automation issues)

--- ISA/features/step_definitions/calabash_steps.rb
+++ ISA/features/step_definitions/calabash_steps.rb
@@ -11,6 +11,19 @@ def p_arry(a)
         i+=1
     end
 end
+def android_textgrep()
+    arr=query("*", :text)
+    act_arr=Array.new
+    i=0
+    while i<arr.length
+      if arr[i]['error'] == nil
+          act_arr.push(arr[i].to_s)
+        else
+      end
+    i+=1
+    end
+    return act_arr
+end
 ##Then Session S1. Display complete text on landing page
 Then(/^Session ([\w\d]+). Display complete-text on ([\w ]+)$/) do |session,pageName|
   if $Configuration[session+"DeviceType"] == "Android"
@@ -25,7 +38,37 @@ Then(/^Session ([\w\d]+). Display complete-text on ([\w ]+)$/) do |session,pageN
     a=a.uniq
     puts a
     elsif pageName == "RatePage"
+    var = android_textgrep()
+    scroll_down
+    sleep 2
+    scroll_down
+    sleep 2
+    var=var+android_textgrep()
+    scroll_down
+    sleep 2
+    scroll_down
+    sleep 2
+    var=var+android_textgrep()
+    scroll_down
+    sleep 2
+    scroll_down
+    sleep 2
+    var=var+android_textgrep()
+    scroll_down
+    sleep 2
+    scroll_down
+    sleep 2
+    var=var+android_textgrep()
+    scroll_down
+    sleep 2
+    scroll_down
+    sleep 2
+    var=var+android_textgrep()
+    var=var.uniq
+    p_arry(var)
     elsif pageName == "Page"
+    var = android_textgrep()
+    p_arry(var)
    end
   else
     sleep 5
@@ -39,6 +82,7 @@ Then(/^Session ([\w\d]+). Display complete-text on ([\w ]+)$/) do |session,pageN
     var=var.uniq
     p_arry(var)
     elsif pageName == "RatePage"
+    begin
     var = query("UILabel", :text)
     scroll("*", :down)
     sleep 2
@@ -62,6 +106,9 @@ Then(/^Session ([\w\d]+). Display complete-text on ([\w ]+)$/) do |session,pageN
     var=var+query("UILabel", :text)
     var=var.uniq
     p_arry(var)
+    rescue => e
+    puts "Fail due to exception: " + e.to_s
+    end
     elsif pageName == "Page"
     var = query("UILabel", :text)
      p_arry(var)
@@ -129,6 +176,43 @@ Then(/^Session ([\w\d]+). Verify Clock from 5th April$/) do |session|
       if $Configuration[session+"DeviceType"] == "Android"
           set_default_device($session[session])
           sleep 3
+                                    ##countdown_type_toggle_button  ##countdown_timer_textview ##countdown_timer_type_textview
+                                    if query("* id:'countdown_timer_type_textview'", :text)[0] == "Days"
+                                    days=query("* id:'countdown_timer_textview'", :text)[0].to_i
+                                    tap_mark "countdown_type_toggle_button"
+                                    end
+                                    sleep 3
+                                    if query("* id:'countdown_timer_type_textview'", :text)[0] == "Hours"
+                                    hours=query("* id:'countdown_timer_textview'", :text)[0].to_i
+                                    tap_mark "countdown_type_toggle_button"
+                                    end
+                                    sleep 3
+                                    if query("* id:'countdown_timer_type_textview'", :text)[0] == "Minutes"
+                                    mins=query("* id:'countdown_timer_textview'", :text)[0].to_i
+                                    tap_mark "countdown_type_toggle_button"
+                                    end
+                                    a=Time.now
+                                    start_date = Date.parse "2015-04-05"
+                                    end_date = Date.parse a.to_s
+                                    act_days = (start_date - end_date).to_i
+                                    puts "Days should be: " + act_days.to_s
+                                    if days == act_days
+                                    puts "Pass"
+                                    else
+                                    fail("Days not correct")
+                                    end
+                                    start_mins = Time.parse "2015-04-06"
+                                    end_mins = Time.parse a.to_s
+                                    act_mins = ((start_mins - end_mins)/60).to_i
+                                    act_hours = (act_mins/60).to_i
+                                    puts "Minutes should be: " + act_mins.to_s
+                                    puts "Hours should be: " + act_hours.to_s
+                                    if mins == act_mins && hours == act_hours
+                                    puts "Pass"
+                                    
+                                    else
+                                    fail("Minutes/Hours not correct")
+                                    end
       else
           sleep 3
           arr=query('UILabel',:text)
